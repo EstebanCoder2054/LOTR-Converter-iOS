5e8e2a8871a48f0fee74e78c23173c4aaffd3d41
Esteban Escobar <esteban.escobar@gorillalogic.com>
2025-01-18 20:01:26 -0500

    Initial Commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Esteban Escobar on 18/01/25.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
