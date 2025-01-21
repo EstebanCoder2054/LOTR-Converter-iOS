77b6f3e7015cf924a0f1981a30e9b9748c737042
Esteban Escobar <esteban.escobar@gorillalogic.com>
2025-01-20 21:43:29 -0500

    added basic project structure

--- LOTRConverter/ContentView.swift
+++ LOTRConverter/ContentView.swift
@@ -9,14 +9,50 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            // Background image
+            Image(.background) // name of the image
+            
+            VStack {
+                // Prancing pony image
+                
+                // Currency exchange text
+                
+                // Conversion section - since this is a compound component it will have more stacks inside of it
+                HStack {
+                    // Left conversion section
+                    VStack{
+                        // Currency
+                        HStack {
+                            // Currency image
+                            
+                            // Currency text
+                        }
+                        
+                        // Text field
+                    }
+                    
+                    // Equal sign
+                    
+                    // Right conversion section
+                    VStack{
+                        // Currency
+                        HStack {
+                            // Currency text
+                            
+                            // Currency image
+                        }
+                        
+                        // Text field
+                    }
+                }
+                
+                // Info button
+            }
         }
-        .padding()
     }
+    
+    
 }
 
 #Preview {
